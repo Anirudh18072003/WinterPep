28c314351f91c80a771d96fc14e3e435483e5219
Anirudh <aniru123987@gmail.com>
2025-01-21 11:35:51 +0530

    added props and destructered

--- Day-5/myapp/src/App.js
+++ Day-5/myapp/src/App.js
@@ -2,9 +2,14 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 
 function App() {
+  const user = {
+    name: "Anirudh",
+    age: "20",
+    country: "India",
+  };
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <Navbar user={user}></Navbar>
     </div>
   );
 }
--- Day-5/myapp/src/Components/Navbar.jsx
+++ Day-5/myapp/src/Components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-export default function Navbar() {
+export default function Navbar(props) {
+  const { user } = props;
   return (
     <>
       <h1>This is my first component</h1>
+      <h5>
+        Hello <strong>{user.name} </strong> your age is {user.age}
+      </h5>
     </>
   );
 }
